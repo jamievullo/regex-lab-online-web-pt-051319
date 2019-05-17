1f3f78cad4d9c39a5c4fb05acf0b574c519a327b
Jamie Vullo <jvullo@protonmail.com>
2019-05-17 12:38:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,6 +23,9 @@ end
 end
 
 def valid_phone_number?(phone)
-if phone.match(/[0-9]{10}\s/) !=nil
-end
+if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+    true
+  else
+    false
+  end
 end 
